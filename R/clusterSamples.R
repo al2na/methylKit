067125c26ed2a278a6ce167a589066d657658897
Sheng Li <shelly1436@gmail.com>
2011-12-06 22:13:34 -0500

    PCASamples now have color plot for both PCA plot and screeplot

--- R/clusterSamples.R
+++ R/clusterSamples.R
@@ -102,17 +102,39 @@
   return(hc)
   }
 
+# Adjust the range of a vector "x" by "i" proportion of the range of x
+# 
+# @param x a vector of value
+# @param i the proprotion of the range of x to adjust the size.
+.adjlim=function(x, i)
+  {
+  if(length(x)>1) {
+    xr=range(x); 
+    xlim=c(); 
+    xlim[1]=xr[1] - abs(xr[1]) * i; 
+    xlim[2]=xr[2] + abs(xr[2] * i); 
+    return(xlim)} 
+  else 
+    print("length vector x should be more than 1")
+  }
 # Principal Components Analysis on methylBase object
 # x matrix each column is a sample
 # cor a logical value indicating whether the calculation should use the correlation matrix or the covariance matrix. (The correlation matrix can only be used if there are no constant variables.)
-.pcaPlot = function(x, cor=TRUE, screeplot=FALSE){
+.pcaPlot = function(x, cor=TRUE, screeplot=FALSE, adj.lim=c(0.001,0.1), treatment=treatment,sample.ids=sample.ids){
   x.pr = princomp(x, cor=cor)
-  if (screeplot)
-    screeplot(x.pr, type="lines", main="CpG dinucleotide methylation PCA Screeplot")
+  if (screeplot){
+    i=5;screeplot(x.pr, type="barplot", main="CpG dinucleotide methylation PCA Screeplot", col = rainbow(i)[i])
+  }
   else{
     loads = loadings(x.pr)
-    plot(loads[,1:2], main = "CpG dinucleotide methylation PCA Analysis")
-    text(loads[,1], loads[,2],adj=c(-0.4,0.3))
+
+    treatment=treatment
+    sample.ids=sample.ids
+    my.cols=rainbow(length(unique(treatment)))
+    
+    plot(loads[,1:2], main = "CpG dinucleotide methylation PCA Analysis",col=my.cols[treatment+1],
+         xlim=.adjlim(loads[,1],adj.lim[1]), ylim=.adjlim(loads[,2], adj.lim[2]))
+    text(loads[,1], loads[,2],labels=sample.ids,adj=c(-0.4,0.3), col=my.cols[treatment+1])
   }
   return(summary(x.pr))
 }
@@ -152,6 +174,7 @@ setMethod("clusterSamples", "methylBase",
 #' @param .Object a \code{methylBase} object
 #' @param cor a logical value indicating whether the calculation should use the correlation matrix or the covariance matrix. (default: TRUE)
 #' @param screeplot a logical value indicating whether to plot the variances against the number of the principal component. (default: FALSE)
+#' @param adj.lim a vector indicating the propotional adjustment of xlim (adj.lim[1]) and ylim (adj.lim[2]). (default: c(0.0004,0.1))
 #'
 #' @return The form of the value returned by \code{PCASamples} is the summary of principal component analysis by \code{princomp}.
 #'
@@ -159,16 +182,16 @@ setMethod("clusterSamples", "methylBase",
 #' @export
 #' @docType methods
 #' @rdname PCASamples-methods
-setGeneric("PCASamples", function(.Object, cor=TRUE, screeplot=FALSE) standardGeneric("PCASamples"))
+setGeneric("PCASamples", function(.Object, cor=TRUE, screeplot=FALSE, adj.lim=c(0.0004,0.1)) standardGeneric("PCASamples"))
 
 #' @rdname PCASamples-methods
 #' @aliases PCASamples,ANY-method
 setMethod("PCASamples", "methylBase",
-                    function(.Object, cor=TRUE, screeplot=FALSE){
+                    function(.Object, cor=TRUE, screeplot=FALSE, adj.lim=c(0.0004,0.1)){
                         meth.mat = getData(.Object)[, .Object@numCs.index]/(.Object[,.Object@numCs.index] + .Object[,.Object@numTs.index] )                                      
                         names(meth.mat)=.Object@sample.ids
                         
-                        .pcaPlot(meth.mat, cor=cor, screeplot=screeplot)
+                        .pcaPlot(meth.mat, cor=cor, screeplot=screeplot, adj.lim=adj.lim, treatment=.Object@treatment,sample.ids=.Object@sample.ids)
                         
                         }
 )
