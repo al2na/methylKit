3b9edf83e74642d6127da5cb34c183034acaa2ce
Sheng Li <shelly1436@gmail.com>
2012-06-07 20:22:34 -0400

    add S4 function fast.fisher.test

--- R/fastFisherTest.R
+++ R/fastFisherTest.R
@@ -1,12 +1,28 @@
-dyn.load(paste("fastFisherTest", .Platform$dynlib.ext, sep=""))
+dyn.load(paste("R/fastFisherTest", .Platform$dynlib.ext, sep=""))
 
-#fast.fisher.test <- function(a,b,c,d) {
-#    n <- as.integer(length(a))
-#    r <- .C("fet",as.integer(a),as.integer(b), as.integer(c), as.integer(d), n, out=double(n))$out
-#    return (r);
-#}
+.First.lib <- function(lib, pkg)
+{
+    library.dynam("LIBNAME", pkg, lib)
+}
 
-fast.fisher.test <- function(x) {
+#' fisher's exact test
+#'
+#' The functions calculate the two-sided pvalue of fisher's exact test. 
+#' @param x a \code{matrix} with four columns, the numCs_sample1, numTs_sample1, numCs_sample2, numTs_sample2 
+#'
+#' @usage fast.fisher.test(x)
+#'
+#' @author Sheng Li and Altuna Akalin
+# @examples
+# library(methylKit)
+# data(methylKit)
+setGeneric("fast.fisher.test", function(x) standardGeneric("fast.fisher.test"))
+
+#' @rdname fast.fisher.test
+#' @aliases fast.fisher.test
+setMethod("fast.fisher.test", "matrix",
+    function(x){
+
     if(is.matrix(x)){
       a=as.integer(x[,1])
       b=as.integer(x[,2])
@@ -19,4 +35,4 @@ fast.fisher.test <- function(x) {
       show("input have to be a matrix with 4 columns")
     }
 
-}
+})
