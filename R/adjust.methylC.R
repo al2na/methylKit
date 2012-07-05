37afe8ff59c7b1932480ddf05bb2227d33e07162
al2na <aakalin@gmail.com>
2012-07-05 12:56:02 -0400

    adjust.methylC is added again

---
+++ R/adjust.methylC.R
@@ -0,0 +1,54 @@
+# 
+#
+
+
+#' adjust measured 5mC levels using 5hmC levels
+#' 
+#' Measured 5mC levels via bisulfite sequencing might be a combination of 5hmC and 5mC levels since bisulfite sequencing can not distinguish
+#' between the two. This function can adjust 5mC levels of a bisulfite sequencing experiment
+#' if the user supplies corresponding 5hmC levels from the same sample.
+#'
+#' @param mc a \code{methylRawList} or \code{methylRaw} containing 5mC levels of a sample or set of samples
+#' @param hmc a \code{methylRawList} or \code{methylRaw} containing 5hmC levels of a sample or set of samples. 
+#'            If a \code{methylRawList} givenm the sample order should be same as "mc" \code{methylRawList} object.
+#'
+#' @return returns adjusted 5-methyl cytosine levels in the form of \code{methylRawList} or \code{methylRaw} object depending on the input object
+#' @usage adjust.methylC(mc,hmc)
+#' @export
+#' @docType methods
+#' @rdname adjust.methylC
+setGeneric("adjust.methylC", function(mc,hmc) standardGeneric("adjust.methylC") )
+
+#' @rdname adjust.methylC
+#' @aliases adjust.methylC,methylRaw,methylRaw-method
+setMethod("adjust.methylC", c("methylRaw","methylRaw"),function(mc,hmc){
+  
+  lst=new("methylRawList",list(mc,hmc),treatment=c(1,0))
+  data=getData(unite(lst))
+  
+  diff=(data$numCs1)-round(data$coverage1*(data$numCs2/data$coverage2))
+  diff[diff<0]=0
+  data$numCs1=diff
+  data$numTs1=data$coverage1-data$numCs1
+  colnames(data)[6:8]=c("coverage","numCs","numTs")
+  new("methylRaw",data[,1:8],sample.id=mc@sample.id,  assembly=mc@assembly, 
+                             context =mc@context,     resolution=mc@resolution)
+  
+})
+
+
+#' @rdname adjust.methylC
+#' @aliases adjust.methylC,methylRawList,methylRawList-method
+setMethod("adjust.methylC", c("methylRawList","methylRawList"),function(mc,hmc){
+  
+  # check lengths equal if not give error
+  if(length(mc) != length(hmc)){stop("lengths of methylRawList objects should be same\n")}
+  
+  my.list=list()
+  for(i in 1:length(mc)){
+    my.list[[i]]=adjust.methylC(mc[[i]],hmc[[i]])
+  }
+  new("methylRawList",my.list,treatment=mc@treatment )
+  
+})
+
