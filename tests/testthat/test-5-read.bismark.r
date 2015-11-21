9443d5df7df52e6e688ae94f3d7297561658d138
alexg9010 <alexgos@zedat.fu-berlin.de>
2015-11-21 20:36:09 +0100

    Update test-5-read.bismark.r

--- tests/testthat/test-5-read.bismark.r
+++ tests/testthat/test-5-read.bismark.r
@@ -7,17 +7,33 @@ test_that("expect that there is an error when reading an unsorted sam file", {
 })
 
 
-test_that("check that CpG context can be read-in", {
-    expect_that( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18"),
-        is_a('methylRaw'))
+test_that("check that CpG context can be read-in as methylRaw", {
+    expect_is( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18"),
+        'methylRaw')
 })
 
-test_that("check that CHG context can be read-in", {
-    expect_that( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18",read.context="CHG"),
-        is_a('methylRaw'))
+test_that("check that CpG context can be read-in as methylRawDB", {
+  expect_is( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18",save.folder="methylDB",save.db=TRUE),
+     'methylRawDB')
 })
 
-test_that("check that CHH context can be read-in", {
-    expect_that( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18",read.context="CHH"),
-        is_a('methylRaw'))
+test_that("check that CHG context can be read-in as methylraw", {
+    expect_is( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18",read.context="CHG"),
+        'methylRaw')
 })
+
+test_that("check that CHG context can be read-in as methylrawdb", {
+  expect_is( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18",read.context="CHG",save.folder="methylDB",save.db=TRUE),
+             'methylRawDB')
+})
+
+test_that("check that CHH context can be read-inas methylraw", {
+    expect_is( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18",read.context="CHH"),
+        'methylRaw')
+})
+
+test_that("check that CHH context can be read-in as methylrawdb", {
+  expect_is( read.bismark(location=system.file("extdata", "test.fastq_bismark.sorted.min.sam", package = "methylKit"),sample.id="test1",assembly="hg18",read.context="CHH",save.folder="methylDB",save.db=TRUE),
+             'methylRawDB')
+})
+
