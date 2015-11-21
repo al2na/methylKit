6fa420f3e43d3951fcc894201c355ca503754a1d
alexg9010 <alexgos@zedat.fu-berlin.de>
2015-11-21 20:48:38 +0100

    add methylDB

--- inst/scripts/create.data.R
+++ inst/scripts/create.data.R
@@ -13,15 +13,12 @@ methylBase.obj=unite(methylRawList.obj)
 
 methylDiff.obj=calculateDiffMeth(methylBase.obj)
 
-db.list=list( system.file("extdata", "test1.txt.bgz", package = "methylKit"),
-                system.file("extdata", "test2.txt.bgz", package = "methylKit"),
-                system.file("extdata", "ctrl1.txt.bgz", package = "methylKit"),
-                system.file("extdata", "ctrl2.txt.bgz", package = "methylKit") )
-
-methylRawListDB.obj=read( db.list,
+methylRawListDB.obj=read( file.list,
                           sample.id=list("test1","test2","ctrl1","ctrl2"),assembly="hg18",treatment=c(1,1,0,0),
-                          dbtype = "tabix")
+                          dbtype = "tabix",dbdir = "methylDB")
 
 methylBaseDB.obj=unite(methylRawListDB.obj)
 
-save(methylRawList.obj,methylBase.obj,methylDiff.obj,methylRawListDB.obj,methylBaseDB.obj,file="methylKit.RData")
+methylDiffDB.obj = calculateDiffMeth(methylBaseDB.obj)
+
+save(methylRawList.obj,methylBase.obj,methylDiff.obj,methylRawListDB.obj,methylBaseDB.obj,methylDiffDB.obj,file="methylKit.RData")
