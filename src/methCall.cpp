50a58897a61677ff5be15dacbd77a7ac4a8f1b06
alexg9010 <alexgos@zedat.fu-berlin.de>
2016-02-04 14:24:16 +0100

    export g++ and gcc

--- src/methCall.cpp
+++ src/methCall.cpp
@@ -1199,7 +1199,7 @@ Rcpp::stop("Feature is not ready yet.\n");
 //     return -1;
 //   }
 
-
+// [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
 void methCall(std::string read1, std::string type="bam", bool nolap=false, int minqual=20,
                 int mincov = 10 , bool phred64 = false , std::string CpGfile ="",
