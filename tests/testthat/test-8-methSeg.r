9bb0d758a5cd1c708e7f9ce4c9f07619bec5c604
Alexander Gosdschan <alexander.gosdschan@mdc-berlin.de>
2017-03-30 09:35:11 +0200

    methSeg and methSeg2bed changes: - in methSeg when only one segment is returned,   put warning, append single segment group and exit before mclust - destrand granges before fastseg to circumvent errors due to strandness - update tests
    - methSeg2bed case handling if no color is sepcified or granges contains only one range
    - methSeg2bed require filename, no default name

--- tests/testthat/test-8-methSeg.r
+++ tests/testthat/test-8-methSeg.r
@@ -11,11 +11,13 @@ test_that("check if methSeg works for methylRaw or methylDiff with resolution re
   expect_is(methSeg(tileDiff,diagnostic.plot = FALSE),"GRanges")
 })
 
-test_that("check if methSeg returns error for methylRaw or methylDiff with resolution base or methylBase objects" ,{
-  expect_error(methSeg(methylRawList.obj[[1]],diagnostic.plot = FALSE))
-  expect_error(methSeg(tileBase,diagnostic.plot = FALSE))
+test_that("check if methSeg works for methylRaw or methylDiff with resolution base or methylBase objects" ,{
+  expect_is(methSeg(methylRawList.obj[[1]],diagnostic.plot = FALSE),"GRanges")
+  expect_is(methSeg(methylDiff.obj,diagnostic.plot = FALSE),"GRanges")
+})
+
+test_that("check if methSeg returns error for methylBase objects" ,{
   expect_error(methSeg(methylBase.obj,diagnostic.plot = FALSE))
-  expect_error(methSeg(methylDiff.obj,diagnostic.plot = FALSE))
 })
 
 seg <- methSeg(tileRaw,diagnostic.plot = FALSE)
@@ -24,3 +26,5 @@ methSeg2bed(segments = seg, filename = "test.bed")
 test_that("check if methSeg2bed returns bed file", {
   expect_true(file.exists("test.bed"))
 })
+
+unlink("test.bed")
