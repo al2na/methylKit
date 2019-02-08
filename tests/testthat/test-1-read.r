ae050463de7bba235a42a1d0b0771c554d29431d
alexg9010 <alex.gos90@gmail.com>
2019-02-08 11:42:41 +0100

    test-1-read: check if methylRawDB is same when directory is given or not, i.e. test wether directory affects object

--- tests/testthat/test-1-read.r
+++ tests/testthat/test-1-read.r
@@ -6,17 +6,23 @@ file.list=list( system.file("extdata", "test1.myCpG.txt", package = "methylKit")
                 system.file("extdata", "control2.myCpG.txt", package = "methylKit") )
 
 myobj=methRead( file.list,
-                sample.id=list("test1","test2","ctrl1","ctrl2"),assembly="hg18",pipeline="amp",treatment=c(1,1,0,0))
+                sample.id=list("test1","test2","ctrl1","ctrl2"),
+		assembly="hg18",pipeline="amp",treatment=c(1,1,0,0))
 
-mydblist = suppressMessages(methRead( file.list,
-               sample.id=list("test1","test2","ctrl1","ctrl2"),assembly="hg18",pipeline="amp",treatment=c(1,1,0,0),dbtype = "tabix",dbdir="methylDB"))
+mydblist = suppressMessages(
+	methRead( file.list,sample.id=list("test1","test2","ctrl1","ctrl2"),
+	assembly="hg18",pipeline="amp",treatment=c(1,1,0,0),
+	dbtype = "tabix",dbdir="methylDB"))
 
-mydblist2 = suppressMessages(methRead( file.list,
-                                      sample.id=list("test1","test2","ctrl1","ctrl2"),assembly="hg18",pipeline="amp",treatment=c(1,1,0,0),dbtype = "tabix"))
+mydblist2 = suppressMessages(
+	methRead( file.list,sample.id=list("test1","test2","ctrl1","ctrl2"),
+	assembly="hg18",pipeline="amp",treatment=c(1,1,0,0),
+	dbtype = "tabix"))
 
 
-mydb = suppressMessages(methRead( mydblist[[1]]@dbpath,
-                                  sample.id="test1",assembly="hg18",dbtype = "tabix",dbdir="methylDB"))
+mydb = suppressMessages(
+	methRead( mydblist[[1]]@dbpath,sample.id="test1",
+	assembly="hg18",dbtype = "tabix",dbdir="methylDB"))
 
 
 test_that("check if there are 4 test files in the file.list",{
@@ -32,7 +38,7 @@ test_that("if methRead return a methylRawListDB", {
 })
 
 test_that("if methRead without given dir return same methylRawListDB", {
-  expect_identical(mydblist,mydblist2)
+  expect_identical(as(mydblist,"methylRawList"),as(mydblist2,"methylRawList"))
 })
 
 test_that("if methRead of database return a methylRawDB", {
