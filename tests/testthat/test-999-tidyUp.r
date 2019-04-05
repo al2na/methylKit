2831581b9eb30f7bbf7baf3dbce05dcb6969a67f
alexg9010 <alex.gos90@gmail.com>
2019-04-05 15:49:56 +0200

    tests: update tidy-up script

--- tests/testthat/test-999-tidyUp.r
+++ tests/testthat/test-999-tidyUp.r
@@ -1,6 +1,7 @@
 context("Tidy Up")
 
-
-unlink(x = "methylDB/",recursive = TRUE)
+dirs <- list.dirs(path = ".",full.names = TRUE,recursive = TRUE)
+dirs <- grep(pattern = "methylDB",x = dirs,value = TRUE)
+unlink(x = dirs, recursive = TRUE)
 unlink("Rplots.pdf")
 unlink("test.bed")
