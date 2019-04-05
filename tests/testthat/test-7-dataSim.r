b7da40bac8b7b60f3e4213b6dbd45a68acd2f8f2
alexg9010 <alex.gos90@gmail.com>
2019-04-05 15:19:12 +0200

    test-datasim: update effect vector

--- tests/testthat/test-7-dataSim.r
+++ tests/testthat/test-7-dataSim.r
@@ -5,7 +5,7 @@ data("methylKit")
 
 # test dataSim
 my.methylBase=dataSim(replicates=4,sites=20000,treatment=c(1,1,0,0))
-my.methylBase2=dataSim(replicates=4,sites=20000,treatment=c(1,1,0,0),effect=c(40,50,60))
+my.methylBase2=dataSim(replicates=4,sites=20000,treatment=c(1,1,0,0),effect=c(40,50,60,70))
 
 test_that("check if dataSim output is a methylBase object", {
   expect_that(my.methylBase, 
