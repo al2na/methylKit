d616c425b0f9e3d3156313293611dd62caf8ead8
alexg9010 <alex.gos90@gmail.com>
2019-08-01 16:52:58 +0200

    move object creation lines to test lines

--- tests/testthat/test-2-unite.r
+++ tests/testthat/test-2-unite.r
@@ -10,36 +10,24 @@ myobj=methRead( file.list,
             pipeline="amp",treatment=c(1,1,0,0))
 
 mydblist = suppressMessages(methRead( file.list,
-                                  sample.id=list("t1","t2","c1","c2"),assembly="hg18",
-                                  pipeline="amp",treatment=c(1,1,0,0),dbtype = "tabix",dbdir="methylDB"))
-
-# unite function
-ta=unite(myobj,destrand=T)
-methidh=unite(myobj)
-methidh2=unite(myobj,min.per.group=1L)
-
-suppressMessages(taDB <- unite(mydblist,destrand=T))
-suppressMessages(methidhDB <- unite(mydblist))
-suppressMessages(methidh2DB <- unite(mydblist,min.per.group=1L))
-
-
-suppressMessages(ta2methylDB <- unite(myobj,destrand=T,save.db = T,dbdir="methylDB"))
-taDB2methyl <- unite(mydblist,destrand=T,save.db = F)
-
+                                  sample.id=list("t1","t2","c1","c2"),
+                                  assembly="hg18", pipeline="amp",
+                                  treatment=c(1,1,0,0), dbtype = "tabix",
+                                  dbdir="methylDB"))
 
 test_that("test if output of unite is  methylBase object", {
-    expect_is(ta, 'methylBase')
-    expect_is(methidh, 'methylBase')
-    expect_is(methidh2, 'methylBase')
-    expect_is(taDB2methyl, 'methylBase')
+    expect_is(unite(myobj,destrand=T), 'methylBase')
+    expect_is(unite(myobj), 'methylBase')
+    expect_is(unite(myobj,min.per.group=1L), 'methylBase')
+    expect_is(unite(mydblist,destrand=T,save.db = F), 'methylBase')
 
 })
 
 test_that("test if output of unite is  methylBaseDB object", {
-  expect_is(taDB, 'methylBaseDB')
-  expect_is(methidhDB, 'methylBaseDB')
-  expect_is(methidh2DB, 'methylBaseDB')
-  expect_is(ta2methylDB, 'methylBaseDB')
+  expect_is(unite(mydblist,destrand=T), 'methylBaseDB')
+  expect_is(unite(mydblist), 'methylBaseDB')
+  expect_is(unite(mydblist,min.per.group=1L), 'methylBaseDB')
+  expect_is(unite(myobj,destrand=T,save.db = T,dbdir="methylDB"), 'methylBaseDB')
 })
 
 
